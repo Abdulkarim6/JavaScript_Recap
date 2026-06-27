3e5a081a1d9d14de3bedb5ff6fbc6525708e3fd2
Abdulkarim6 <mdhossainjwel687@gmail.com>
2026-06-28 00:04:32 +0600

    ObjectOperation: finding keys, values, and entries of object

--- objectOperations.js
+++ objectOperations.js
@@ -5,5 +5,19 @@ const std1 = {
 };
 
 for(let key in std1){
-    console.log(key,": ", std1[key]);
+    //console.log(key,": ", std1[key]);
 }
+
+const keys = Object.keys(std1);
+// output: ["name", "age", "dpt"];
+
+const values = Object.values(std1);
+// output:  ['Karim', 25, 'CSE'];
+
+const pair = Object.entries(std1);
+// output: [2) [['name', 'Karim'],  ['age', 25],  ['dpt', 'CSE']];
+
+std1["roll"] = "10023";
+//output: {name: 'Karim', age: 25, dpt: 'CSE', roll: '10023'}
+
+console.log(pair);
